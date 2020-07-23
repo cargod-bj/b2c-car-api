143460acc13a92790c63b31b8ce3aea2d4db24c7
naite.zhou <naite.zhou@mljr.com>
2020-07-23 15:54:04 +0800

    删除enum中无效的text等信息

--- enums/enum.go
+++ enums/enum.go
@@ -1,203 +1,117 @@
 package enums
 
-import (
-	"sort"
-)
-
 const Rate float32 = 0.04
 
 const (
-	FuelTypePetrol         = 1
-	FuelTypePetrolWithNGV  = 2
-	FuelTypeDiesel         = 3
-	FuelTypeDieselWithNGV  = 4
-	FuelTypeHybridElectric = 5
+	color uint32 = iota
+	ColorBlack
+	ColorWhite
+	ColorGray
+	ColorSilver
+	ColorRed
+	ColorBlue
+	ColorBrown
+	ColorGold
+	ColorGreen
+	ColorOrange
+	ColorBeige
+	ColorPurple
+	ColorBronze
+	ColorOther
 )
 
-var fuelTypeText = map[int]string{
-	FuelTypePetrol:         "Petrol",
-	FuelTypePetrolWithNGV:  "Petrol with NGV",
-	FuelTypeDiesel:         "Diesel",
-	FuelTypeDieselWithNGV:  "Diesel with NGV",
-	FuelTypeHybridElectric: "Hybrid/Electric",
+// v 是否是合法的transmission
+func IsColor(v uint32) bool {
+	return v == TransmissionManual || v == TransmissionAuto
 }
 
-// 附件类型
+// 变速箱
 const (
-	EnclosureTypeZero uint32 = iota
-	// 整备单据
-	EnclosureTypeRecondition
-)
-
-func FuelTypeText(code int) string {
-	return fuelTypeText[code]
-}
-
-func FuelTypeList() []int {
-	return getKeys(fuelTypeText)
-}
-
-func getKeys(maps map[int]string) []int {
-	keys := make([]int, 0, len(maps))
-	for mapKey, _ := range maps {
-		keys = append(keys, mapKey)
-	}
-	sort.Ints(keys)
-
-	return keys
-}
-
-var fuelTypeCode = map[string]int{
-	"Petrol":          FuelTypePetrol,
-	"Petrol with NGV": FuelTypePetrolWithNGV,
-	"Diesel":          FuelTypeDiesel,
-	"Diesel with NGV": FuelTypeDieselWithNGV,
-	"Hybrid/Electric": FuelTypeHybridElectric,
-}
-
-func FuelTypeCode(text string) int {
-	return fuelTypeCode[text]
-}
-
-const (
-	ColorBlack  = 1
-	ColorBlue   = 2
-	ColorBrown  = 3
-	ColorGold   = 4
-	ColorGray   = 5
-	ColorGreen  = 6
-	ColorOrange = 7
-	ColorPurple = 8
-	ColorRed    = 9
-	ColorSliver = 10
-	ColorTan    = 11
-	ColorWhite  = 12
-)
-
-var colorText = map[int]string{
-	ColorBlack:  "Black",
-	ColorBlue:   "Blue",
-	ColorBrown:  "Brown",
-	ColorGold:   "Gold",
-	ColorGray:   "Gray",
-	ColorGreen:  "Green",
-	ColorOrange: "Orange",
-	ColorPurple: "Purple",
-	ColorRed:    "Red",
-	ColorSliver: "Sliver",
-	ColorTan:    "Tan",
-	ColorWhite:  "White",
-}
-
-func ColorText(code int) string {
-	return colorText[code]
-}
-func ColorList() []int {
-	return getKeys(colorText)
-}
-
-var colorCode = map[string]int{
-	"Black":  ColorBlack,
-	"Blue":   ColorBlue,
-	"Brown":  ColorBrown,
-	"Gold":   ColorGold,
-	"Gray":   ColorGray,
-	"Green":  ColorGreen,
-	"Orange": ColorOrange,
-	"Purple": ColorPurple,
-	"Red":    ColorRed,
-	"Sliver": ColorSliver,
-	"Tan":    ColorTan,
-	"White":  ColorWhite,
-}
-
-func ColorCode(text string) int {
-	return colorCode[text]
-}
-
-const (
+	// 变速箱：手动
 	TransmissionManual = 1
-	TransmissionAuto   = 2
+	// 变速箱：自动
+	TransmissionAuto = 2
 )
 
-var transmissionText = map[int]string{
-	TransmissionManual: "Manual",
-	TransmissionAuto:   "Auto",
+// v 是否是合法的transmission
+func IsTransmission(v uint32) bool {
+	return v == TransmissionManual || v == TransmissionAuto
 }
 
-func TransmissionText(code int) string {
-	return transmissionText[code]
-}
-
-func TransmissionList() []int {
-	return getKeys(transmissionText)
-}
-
-var transmissionCode = map[string]int{
-	"Manual": TransmissionManual,
-	"Auto":   TransmissionAuto,
-}
-
-func TransmissionCode(text string) int {
-	return transmissionCode[text]
-}
-
+// 注册类型
 const (
+	// 注册类型: 私家
 	RegistrationTypePrivate = 1
+	// 注册类型: 公司
 	RegistrationTypeCompany = 2
 )
 
-var registrationTypeText = map[int]string{
-	RegistrationTypePrivate: "Private",
-	RegistrationTypeCompany: "Company",
-}
-
-func RegistrationTypeText(code int) string {
-	return registrationTypeText[code]
-}
-
-func RegistrationTypeList() []int {
-	return getKeys(registrationTypeText)
-}
-
-var registrationTypeCode = map[string]int{
-	"Private": RegistrationTypePrivate,
-	"Company": RegistrationTypeCompany,
-}
-
-func RegistrationTypeCode(text string) int {
-	return registrationTypeCode[text]
+// v 是否是合法的RegistrationType
+func IsRegistrationType(v uint32) bool {
+	return v == RegistrationTypePrivate || v == RegistrationTypeCompany
 }
 
 // 库存状态：1在库，2离库
 const (
-	InventoryStatusIn  = 1
+	// 库存状态：在库
+	InventoryStatusIn = 1
+	// 库存状态：离库
 	InventoryStatusNot = 2
 )
 
+// v 是否是合法的InventoryStatus
+func IsInventoryStatus(v uint32) bool {
+	return v == InventoryStatusIn || v == InventoryStatusNot
+}
+
 // 车辆状态：100 待整备，110整备中，120待核查，200待交易，210交易中，220已出售
 const (
+	// 车辆状态: 待整备
 	CarStateToDoRecondition = 100
-	CarStateReconditioning  = 110
-	CarStateUnchecked       = 120
-	CarStateWaitingTrade    = 200
-	CarStateTrading         = 210
-	CarStateTraded          = 220
+	// 车辆状态: 整备中
+	CarStateReconditioning = 110
+	// 车辆状态: 待核查
+	CarStateUnchecked = 120
+	// 车辆状态: 待交易
+	CarStateWaitingTrade = 200
+	// 车辆状态: 交易中
+	CarStateTrading = 210
+	// 车辆状态: 已出售
+	CarStateTraded = 220
 )
+
+// v 是否是合法的CarState
+func IsCarState(v uint32) bool {
+	return v == CarStateToDoRecondition || v == CarStateReconditioning ||
+		v == CarStateUnchecked || v == CarStateWaitingTrade ||
+		v == CarStateTrading || v == CarStateTraded
+}
 
 // 上架状态：1未上架，2已上架
 const (
+	// 上架状态: 未上架
 	SaleStatusOffline = 1
-	SaleStatusOnline  = 2
+	// 上架状态: 已上架
+	SaleStatusOnline = 2
 )
+
+// v 是否是合法的SaleStatus
+func IsSaleStatus(v uint32) bool {
+	return v == SaleStatusOffline || v == SaleStatusOnline
+}
 
 // 经销商类型：2 Carsome，3 Partner
 const (
-	// 经销商类型：0自营
+	// 经销商类型：自营
 	DealerTypeCarsome = 2
-	// 经销商类型：1第三方
+	// 经销商类型：第三方
 	DealerTypePartner = 3
 )
+
+// v 是否是合法的DealerType
+func IsDealerType(v uint32) bool {
+	return v == DealerTypeCarsome || v == DealerTypePartner
+}
 
 // 枚举类型名称，对应car_enums_config表中的type字段
 const (
@@ -225,11 +139,16 @@ const (
 
 // 车辆资源类型枚举
 const (
+	carRes uint32 = iota
 	// 车辆资源：内部照片
-	CarRes uint32 = iota
 	CarResImgInner
 	// 车辆资源：外部照片
 	CarResImgOuter
 	// 整备单据
 	CarResReconditionFiles = 10
 )
+
+// v 是否是合法的CarRes
+func IsCarRes(v uint32) bool {
+	return v == CarResImgInner || v == CarResImgOuter || v == CarResReconditionFiles
+}
