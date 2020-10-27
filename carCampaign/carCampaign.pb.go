3861a38016375b7d45d4dee6fd1a14fbdf9445fd
ypq <374563453@qq.com>
2020-10-27 16:49:08 +0800

    限时活动

--- carCampaign/carCampaign.pb.go
+++ carCampaign/carCampaign.pb.go
@@ -2,9 +2,9 @@
 // versions:
 // 	protoc-gen-go v1.24.0
 // 	protoc        v3.9.0
-// source: carCampaign/carCompaign.proto
+// source: carCampaign/carCampaign.proto
 
-package carCompaign
+package carCampaign
 
 import (
 	common "github.com/cargod-bj/b2c-proto-common/common"
@@ -45,7 +45,7 @@ type CarCampaignDto struct {
 func (x *CarCampaignDto) Reset() {
 	*x = CarCampaignDto{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_carCampaign_carCompaign_proto_msgTypes[0]
+		mi := &file_carCampaign_carCampaign_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -58,7 +58,7 @@ func (x *CarCampaignDto) String() string {
 func (*CarCampaignDto) ProtoMessage() {}
 
 func (x *CarCampaignDto) ProtoReflect() protoreflect.Message {
-	mi := &file_carCampaign_carCompaign_proto_msgTypes[0]
+	mi := &file_carCampaign_carCampaign_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -71,7 +71,7 @@ func (x *CarCampaignDto) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use CarCampaignDto.ProtoReflect.Descriptor instead.
 func (*CarCampaignDto) Descriptor() ([]byte, []int) {
-	return file_carCampaign_carCompaign_proto_rawDescGZIP(), []int{0}
+	return file_carCampaign_carCampaign_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *CarCampaignDto) GetId() uint64 {
@@ -116,12 +116,12 @@ func (x *CarCampaignDto) GetResH5FilterTagUnselected() string {
 	return ""
 }
 
-var File_carCampaign_carCompaign_proto protoreflect.FileDescriptor
+var File_carCampaign_carCampaign_proto protoreflect.FileDescriptor
 
-var file_carCampaign_carCompaign_proto_rawDesc = []byte{
+var file_carCampaign_carCampaign_proto_rawDesc = []byte{
 	0x0a, 0x1d, 0x63, 0x61, 0x72, 0x43, 0x61, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x2f, 0x63, 0x61,
-	0x72, 0x43, 0x6f, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
-	0x0b, 0x63, 0x61, 0x72, 0x43, 0x6f, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x1a, 0x39, 0x67, 0x69,
+	0x72, 0x43, 0x61, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12,
+	0x0b, 0x63, 0x61, 0x72, 0x43, 0x61, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x1a, 0x39, 0x67, 0x69,
 	0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x61, 0x72, 0x67, 0x6f, 0x64, 0x2d,
 	0x62, 0x6a, 0x2f, 0x62, 0x32, 0x63, 0x2d, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x2d, 0x63, 0x6f, 0x6d,
 	0x6d, 0x6f, 0x6e, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2f, 0x63, 0x6f, 0x6d, 0x6d, 0x6f,
@@ -144,37 +144,37 @@ var file_carCampaign_carCompaign_proto_rawDesc = []byte{
 	0x55, 0x6e, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x65, 0x64, 0x18, 0x07, 0x20, 0x01, 0x28, 0x09,
 	0x52, 0x18, 0x52, 0x65, 0x73, 0x48, 0x35, 0x46, 0x69, 0x6c, 0x74, 0x65, 0x72, 0x54, 0x61, 0x67,
 	0x55, 0x6e, 0x73, 0x65, 0x6c, 0x65, 0x63, 0x74, 0x65, 0x64, 0x32, 0x4c, 0x0a, 0x0b, 0x43, 0x61,
-	0x72, 0x43, 0x6f, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x12, 0x3d, 0x0a, 0x18, 0x47, 0x65, 0x74,
+	0x72, 0x43, 0x61, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x12, 0x3d, 0x0a, 0x18, 0x47, 0x65, 0x74,
 	0x43, 0x75, 0x72, 0x72, 0x65, 0x6e, 0x74, 0x41, 0x63, 0x74, 0x69, 0x76, 0x65, 0x43, 0x61, 0x6d,
 	0x70, 0x61, 0x69, 0x67, 0x6e, 0x12, 0x0d, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x49,
 	0x64, 0x44, 0x74, 0x6f, 0x1a, 0x10, 0x2e, 0x63, 0x6f, 0x6d, 0x6d, 0x6f, 0x6e, 0x2e, 0x52, 0x65,
 	0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x22, 0x00, 0x42, 0x2e, 0x5a, 0x2c, 0x67, 0x69, 0x74, 0x68,
 	0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x63, 0x61, 0x72, 0x67, 0x6f, 0x64, 0x2d, 0x62, 0x6a,
 	0x2f, 0x62, 0x32, 0x63, 0x2d, 0x63, 0x61, 0x72, 0x2d, 0x61, 0x70, 0x69, 0x2f, 0x63, 0x61, 0x72,
-	0x43, 0x6f, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x43, 0x61, 0x6d, 0x70, 0x61, 0x69, 0x67, 0x6e, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_carCampaign_carCompaign_proto_rawDescOnce sync.Once
-	file_carCampaign_carCompaign_proto_rawDescData = file_carCampaign_carCompaign_proto_rawDesc
+	file_carCampaign_carCampaign_proto_rawDescOnce sync.Once
+	file_carCampaign_carCampaign_proto_rawDescData = file_carCampaign_carCampaign_proto_rawDesc
 )
 
-func file_carCampaign_carCompaign_proto_rawDescGZIP() []byte {
-	file_carCampaign_carCompaign_proto_rawDescOnce.Do(func() {
-		file_carCampaign_carCompaign_proto_rawDescData = protoimpl.X.CompressGZIP(file_carCampaign_carCompaign_proto_rawDescData)
+func file_carCampaign_carCampaign_proto_rawDescGZIP() []byte {
+	file_carCampaign_carCampaign_proto_rawDescOnce.Do(func() {
+		file_carCampaign_carCampaign_proto_rawDescData = protoimpl.X.CompressGZIP(file_carCampaign_carCampaign_proto_rawDescData)
 	})
-	return file_carCampaign_carCompaign_proto_rawDescData
+	return file_carCampaign_carCampaign_proto_rawDescData
 }
 
-var file_carCampaign_carCompaign_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_carCampaign_carCompaign_proto_goTypes = []interface{}{
-	(*CarCampaignDto)(nil),  // 0: carCompaign.CarCampaignDto
+var file_carCampaign_carCampaign_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_carCampaign_carCampaign_proto_goTypes = []interface{}{
+	(*CarCampaignDto)(nil),  // 0: carCampaign.CarCampaignDto
 	(*common.IdDto)(nil),    // 1: common.IdDto
 	(*common.Response)(nil), // 2: common.Response
 }
-var file_carCampaign_carCompaign_proto_depIdxs = []int32{
-	1, // 0: carCompaign.CarCompaign.GetCurrentActiveCampaign:input_type -> common.IdDto
-	2, // 1: carCompaign.CarCompaign.GetCurrentActiveCampaign:output_type -> common.Response
+var file_carCampaign_carCampaign_proto_depIdxs = []int32{
+	1, // 0: carCampaign.CarCampaign.GetCurrentActiveCampaign:input_type -> common.IdDto
+	2, // 1: carCampaign.CarCampaign.GetCurrentActiveCampaign:output_type -> common.Response
 	1, // [1:2] is the sub-list for method output_type
 	0, // [0:1] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -182,13 +182,13 @@ var file_carCampaign_carCompaign_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_carCampaign_carCompaign_proto_init() }
-func file_carCampaign_carCompaign_proto_init() {
-	if File_carCampaign_carCompaign_proto != nil {
+func init() { file_carCampaign_carCampaign_proto_init() }
+func file_carCampaign_carCampaign_proto_init() {
+	if File_carCampaign_carCampaign_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_carCampaign_carCompaign_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_carCampaign_carCampaign_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*CarCampaignDto); i {
 			case 0:
 				return &v.state
@@ -205,18 +205,18 @@ func file_carCampaign_carCompaign_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_carCampaign_carCompaign_proto_rawDesc,
+			RawDescriptor: file_carCampaign_carCampaign_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
-		GoTypes:           file_carCampaign_carCompaign_proto_goTypes,
-		DependencyIndexes: file_carCampaign_carCompaign_proto_depIdxs,
-		MessageInfos:      file_carCampaign_carCompaign_proto_msgTypes,
+		GoTypes:           file_carCampaign_carCampaign_proto_goTypes,
+		DependencyIndexes: file_carCampaign_carCampaign_proto_depIdxs,
+		MessageInfos:      file_carCampaign_carCampaign_proto_msgTypes,
 	}.Build()
-	File_carCampaign_carCompaign_proto = out.File
-	file_carCampaign_carCompaign_proto_rawDesc = nil
-	file_carCampaign_carCompaign_proto_goTypes = nil
-	file_carCampaign_carCompaign_proto_depIdxs = nil
+	File_carCampaign_carCampaign_proto = out.File
+	file_carCampaign_carCampaign_proto_rawDesc = nil
+	file_carCampaign_carCampaign_proto_goTypes = nil
+	file_carCampaign_carCampaign_proto_depIdxs = nil
 }
