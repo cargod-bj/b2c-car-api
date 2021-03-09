b2ffe3b3ad97d150086204c68b108983434ab49b
Joseph <362489140@qq.com>
2021-03-09 11:49:25 +0800

    test

--- carProto/car.pb.micro.go
+++ carProto/car.pb.micro.go
@@ -83,6 +83,10 @@ type CarService interface {
 	GetCarDetailAccessData(ctx context.Context, in *CarNoReq, opts ...client.CallOption) (*common.Response, error)
 	// 获取指定carNo或licensePlate的车辆：返回 data: CarDto
 	GetCarByCarNoOrLicensePlate(ctx context.Context, in *CarNoOrLicensePlateDto, opts ...client.CallOption) (*common.Response, error)
+	//  车辆变更门店
+	TransferStore(ctx context.Context, in *CarTransferStore, opts ...client.CallOption) (*common.Response, error)
+	// 车辆变更门店记录
+	TransferStoreList(ctx context.Context, in *CarTransferStoreList, opts ...client.CallOption) (*common.Response, error)
 }
 
 type carService struct {
@@ -287,6 +291,26 @@ func (c *carService) GetCarByCarNoOrLicensePlate(ctx context.Context, in *CarNoO
 	return out, nil
 }
 
+func (c *carService) TransferStore(ctx context.Context, in *CarTransferStore, opts ...client.CallOption) (*common.Response, error) {
+	req := c.c.NewRequest(c.name, "Car.TransferStore", in)
+	out := new(common.Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *carService) TransferStoreList(ctx context.Context, in *CarTransferStoreList, opts ...client.CallOption) (*common.Response, error) {
+	req := c.c.NewRequest(c.name, "Car.TransferStoreList", in)
+	out := new(common.Response)
+	err := c.c.Call(ctx, req, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // Server API for Car service
 
 type CarHandler interface {
@@ -329,6 +353,10 @@ type CarHandler interface {
 	GetCarDetailAccessData(context.Context, *CarNoReq, *common.Response) error
 	// 获取指定carNo或licensePlate的车辆：返回 data: CarDto
 	GetCarByCarNoOrLicensePlate(context.Context, *CarNoOrLicensePlateDto, *common.Response) error
+	//  车辆变更门店
+	TransferStore(context.Context, *CarTransferStore, *common.Response) error
+	// 车辆变更门店记录
+	TransferStoreList(context.Context, *CarTransferStoreList, *common.Response) error
 }
 
 func RegisterCarHandler(s server.Server, hdlr CarHandler, opts ...server.HandlerOption) error {
@@ -352,6 +380,8 @@ func RegisterCarHandler(s server.Server, hdlr CarHandler, opts ...server.Handler
 		GetCarByNoFuzzy(ctx context.Context, in *CarNoDto, out *common.Response) error
 		GetCarDetailAccessData(ctx context.Context, in *CarNoReq, out *common.Response) error
 		GetCarByCarNoOrLicensePlate(ctx context.Context, in *CarNoOrLicensePlateDto, out *common.Response) error
+		TransferStore(ctx context.Context, in *CarTransferStore, out *common.Response) error
+		TransferStoreList(ctx context.Context, in *CarTransferStoreList, out *common.Response) error
 	}
 	type Car struct {
 		car
@@ -438,4 +468,12 @@ func (h *carHandler) GetCarDetailAccessData(ctx context.Context, in *CarNoReq, o
 
 func (h *carHandler) GetCarByCarNoOrLicensePlate(ctx context.Context, in *CarNoOrLicensePlateDto, out *common.Response) error {
 	return h.CarHandler.GetCarByCarNoOrLicensePlate(ctx, in, out)
+}
+
+func (h *carHandler) TransferStore(ctx context.Context, in *CarTransferStore, out *common.Response) error {
+	return h.CarHandler.TransferStore(ctx, in, out)
+}
+
+func (h *carHandler) TransferStoreList(ctx context.Context, in *CarTransferStoreList, out *common.Response) error {
+	return h.CarHandler.TransferStoreList(ctx, in, out)
 }
